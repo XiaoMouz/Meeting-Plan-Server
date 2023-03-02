eb7861e2306d22b1ca09d7658e280f751abff486
XiaoMouz <gxiaomouz@gmail.com>
2023-03-02 20:52:35 +0800

    chore: base get info interface finish

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db/connection');
+
+// get resource table from database
+getTables = (req, res, tableName) => {
+    var sql = "select * from " + tableName
+    var sqlArr = []
+    var callBack = (err, data) => {
+        if (err) {
+            console.error("Error: Fuck up in index.js > Function getRes > callBack failed, case:" + err)
+        }
+        else {
+            res.send(data)
+        }
+    }
+    db.sqlConnect(sql, sqlArr, callBack)
+}
+
+router.get('/info/res', (req, res) => getTables(req, res, "resources"))
+router.get('/info/notice', (req, res) => getTables(req, res, "notices"))
 
 
-router.get('/test', (req, res, next) => {
-
+/* Test Functions */
+router.get('/', (req, res, next) => {
+    res.send("Hi,there is your request info:\n" +
+        "req.method: " + req.method +
+        "\nreq.url: " + req.url
+    )
 });
 
-router.post('/test', (req, res, next) => {
+router.post('/', (req, res, next) => {
     res.send("Hi,there is your request info:\n" +
         "req.method: " + req.method +
         "\nreq.url: " + req.url +
@@ -14,8 +37,5 @@ router.post('/test', (req, res, next) => {
     )
 });
 
-router.all('*', (req, res, next) => {
-    res.send("You maybe request a wrong path, please check our document.")
-});
 
 module.exports = router;
