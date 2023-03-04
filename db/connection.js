635ff5c44d1854311ea2d6756abc1e4b99bf5a96
XiaoMouz <gxiaomouz@gmail.com>
2023-03-05 02:19:09 +0800

    style: "some content trouble"

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql')
 const dbConfig = require('../config/db-config.json')
 
 module.exports = {
-    //数据库配置
+    // 数据库配置
     config: {
         host: dbConfig.host,
         port: dbConfig.port,
@@ -10,8 +10,8 @@ module.exports = {
         password: dbConfig.password,
         database: dbConfig.database
     },
-    //连接数据库
-    //连接池方法
+    // 连接数据库
+    // 连接池方法
     sqlConnect: function (sql, sqlArr, callBack) {
         var pool = mysql.createPool(this.config)
         pool.getConnection((err, conn) => {
@@ -19,14 +19,14 @@ module.exports = {
                 console.log('连接失败')
                 return
             }
-            //事件驱动回调
+            // 事件驱动回调
             conn.query(sql, sqlArr, callBack)
             conn.release()
         })
     },
 
-    //异步连接池
-    //promise 回调
+    // 异步连接池
+    // promise 回调
     SySqlConnect: function (sySql, sqlArr) {
         return new Promise((resolve, reject) => {
             var pool = mysql.createPool(this.config)
