ae85ce2ace56c78204dcb2cc0e6020c4f1ad838f
XiaoMouz <gxiaomouz@gmail.com>
2023-03-05 02:20:44 +0800

    chore: "push interface - user"

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,60 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db/connection');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.send("now is user path")
+/**
+ * @param {Object} req request
+ * @param {Object} res response
+ * @param {String} sql request sql
+ * @param {Array} paramArr the parameters of sql
+ * @returns {void}
+ * @description get data from database
+ */
+getData = (req, res, sql, paramArr) => {
+    if (paramArr == null) {
+        paramArr = []
+    }
+    var callBack = (err, data) => {
+        if (err) {
+            console.error("Error(user.js): Fuck up callBack failed, case:" + err)
+        }
+        else {
+            return data
+        }
+    }
+    db.sqlConnect(sql, paramArr, callBack)
+};
+
+/**
+ * path: /user/test
+ * @description test the interface work statu
+ */
+router.get('/test', function (req, res, next) {
+    res.send({ code: 1, msg: "test success" })
+});
+
+
+router.post('/login', function (req, res, next) {
+    var sql = "select * from users where username = ?"
+    var paramArr = [req.body.username]
+
+    // check the request
+    if (req.body.username == null || req.body.password == null) {
+        return res.send({ code: 0, msg: "request is null" })
+    }
+
+    // check the user
+    var user = getData(req, res, sql, paramArr)
+    
+    if (user == null) {
+        return res.send({ code: 0, msg: "user not exist" })
+    }
+    if (user.password != req.body.password) {
+        return res.send({ code: 0, msg: "password error" })
+    }
+
+
+    return res.send({ code: 1, msg: "login success" })
 });
 
 
